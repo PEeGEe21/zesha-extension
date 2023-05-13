83bae9d144d3a5ba4fd69405aae6c820a8e0c9a6
= <=>
2023-05-13 22:42:10 +0100

    created pages and updated home an history page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Redirect,
+} from 'react-router-dom';
+
+import Test from './pages/Home';
+import Settings from './pages/Settings';
+import Videos from './pages/Videos';
+import History from './pages/History';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Test />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
+    </Router>
   );
 }
 
